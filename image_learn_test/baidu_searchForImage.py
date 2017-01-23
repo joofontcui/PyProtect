3a8a29b0599aca4571e30d051b9277026df3ff36
cuijun <cuijun@10.2.138.61>
2017-01-23 16:19:03 +0800

    update spider

--- image_learn_test/baidu_searchForImage.py
+++ image_learn_test/baidu_searchForImage.py
@@ -7,14 +7,20 @@
 # @File    : baidu_searchForImage.py
 # @Software: PyCharm
 #---------------------------------------
+
 '''
-    抓取百度图片一个词条瀑布图中，前150条
+    抓取百度图片xx词条瀑布图中，前yy条
 '''
 import json
 import os
+import socket
 import urllib
 import requests
 
+#全局超时：5s
+socket.setdefaulttimeout(5.0)
+
+#用户交互
 xx = raw_input("输入你要检索的字段,例如->男人装:") #输入你要检索的字段,例如：男人装
 zz = urllib.quote(xx)
 print zz
@@ -22,7 +28,7 @@ yy = input("输入你要下载的图片总数，例如->1000:") #输入你要下
 print yy
 
 for j in range(0, yy, 30):
-    print type(j)
+    #解析url，拼装动态接口，获取数据，转成json
     jj = str(j)
     print type(jj)
     baiduUrl = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result" \
@@ -44,20 +50,49 @@ for j in range(0, yy, 30):
     buf=requests.get(baiduUrl, headers=headers, verify=False).content
     to_json = json.loads(buf)
     print to_json
-    # urllist=re.findall(r'src="(http.+?\.jpg)"',buf,re.I)
 
+    #打开或新建文件，用来放置抓取的图片
     filepath=os.getcwd()+'\imagesForBaidu'
     if os.path.exists(filepath) is False:
         os.mkdir(filepath)
 
+    #主要逻辑：百度瀑布图接口数据不固定，原图应该在replaceUrl里，如果没有->直接拿百度的缩略图，如果有判断里面有几个值，尽量取第二个（最新的）
+    '''示例
+        "replaceUrl": [
+                {
+                    "ObjURL": "http://img2.imgtn.bdimg.com/it/u=2310040820,4200770776&fm=214&gp=0.jpg",
+                    "FromURL": "http://tu.pcpop.com/all-655385.htm"
+                },
+                {
+                    "ObjURL": "http://img5.pcpop.com/articleimages/picshow/120x90/20110425/2011042522334710762.jpg",
+                    "FromURL": "http://tu.pcpop.com/pic-655385-14.htm"
+                }
+            ],
+    '''
     x = 1
     for i in to_json['data']:
         if len(i) == 0:
-            print '空的,我直接干掉'
+            print '空值,抛弃'
         else:
             temp = filepath + '\%s.jpg' % int(x+j)
             print u'正在下载第%s张图片' % int(x+j)
-            # print i["thumbURL"]
-            urllib.urlretrieve(i["thumbURL"], temp)
+            if 'replaceUrl' not in i:
+                print i["thumbURL"]
+                urllib.urlretrieve(i["thumbURL"].strip(), temp)
+            else:
+                try:
+                    if len(i["replaceUrl"]) == 2:
+                        print i["replaceUrl"][1]['ObjURL']
+                        urllib.urlretrieve(i["replaceUrl"][1]['ObjURL'].strip(), temp)
+                    elif len(i["replaceUrl"]) == 1:
+                        print i["replaceUrl"][0]['ObjURL']
+                        urllib.urlretrieve(i["replaceUrl"][0]['ObjURL'].strip(), temp)
+                    elif len(i["replaceUrl"]) == 0:
+                        urllib.urlretrieve(i["thumbURL"].strip(), temp)
+                    else:
+                        print '空值,抛弃';
+                except:
+                    print '抛出一个异常'
             x += 1
+
 print u'图片下载完毕，保存路径为'+filepath
